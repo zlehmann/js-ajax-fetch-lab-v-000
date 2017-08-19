7aae955b71501f2d65c5f79a583f34ac22f7c041
Gabe Jackson <gj@mail.co.de>
2017-08-19 13:00:33 -0400

    Add back solution

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
+const baseURL = 'https://api.github.com';
+const user = 'maxwellbenton';
+
+function getIssues() {
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
+function createIssue() {
+  const repo = `${user}/js-ajax-fetch-lab`;
+  const url = `${baseURL}/repos/${repo}/issues`;
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => getIssues());
+}
+
+function showResults(json) {
+  document.getElementById('results').innerHTML = `<a href=${json.html_url}>${
+    json.html_url
+  }</a>`;
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
+  const url = `${baseURL}/repos/${repo}/forks`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showResults(json));
 }
 
-function showResults(json) {
-  //use this function to display the results from forking via the API
-}
-
-function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-}
-
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return '';
 }
