e585603f9baf48e32f0ddb3da6b4b89efc1aeb8b
maxwellbenton <maxwellbenton@gmail.com>
2018-09-06 10:40:53 -0400

    updated solution

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const baseURL = 'https://api.github.com';
-<<<<<<< HEAD
 const user = '<YOUR_USERNAME>';
 
 function getToken() {
@@ -13,43 +12,12 @@ function forkRepo() {
   const url = `${baseURL}/repos/${repo}/forks`;
   fetch(url, {
     method: 'POST',
-=======
-const user = 'maxwellbenton';
-
-function getIssues() {
-  const repo = `${user}/js-ajax-fetch-lab`;
-  const url = `${baseURL}/repos/${repo}/issues`;
-  fetch(url, {
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
     .then(res => res.json())
-    .then(json => console.log(json));
-}
-
-function createIssue() {
-  const repo = `${user}/js-ajax-fetch-lab`;
-  const url = `${baseURL}/repos/${repo}/issues`;
-  const postData = {
-    title: document.getElementById('title').value,
-    body: document.getElementById('body').value
-  };
-
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(postData),
->>>>>>> Add back solution
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-  })
-    .then(res => res.json())
-<<<<<<< HEAD
     .then(json => showResults(json));
-=======
-    .then(json => getIssues());
->>>>>>> Add back solution
 }
 
 function showResults(json) {
@@ -58,7 +26,6 @@ function showResults(json) {
   }</a>`;
 }
 
-<<<<<<< HEAD
 function createIssue() {
   const repo = `${user}/js-ajax-fetch-lab`;
   const url = `${baseURL}/repos/${repo}/issues`;
@@ -70,19 +37,11 @@ function createIssue() {
   fetch(url, {
     method: 'POST',
     body: JSON.stringify(postData),
-=======
-function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  const url = `${baseURL}/repos/${repo}/forks`;
-  fetch(url, {
-    method: 'POST',
->>>>>>> Add back solution
     headers: {
       Authorization: `token ${getToken()}`
     }
   })
     .then(res => res.json())
-<<<<<<< HEAD
     .then(json => getIssues());
 }
 
@@ -96,13 +55,4 @@ function getIssues() {
   })
     .then(res => res.json())
     .then(json => console.log(json));
-=======
-    .then(json => showResults(json));
-}
-
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
->>>>>>> Add back solution
 }
