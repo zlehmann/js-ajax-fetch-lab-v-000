2a868da1deea9bd27d1c3d11c86d690ce8f6407f
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 16:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function forkRepo() {
   //use fetch to fork it!
   fetch(repo, {
     headers: {
-      'Authorization': 'token 1eb92b31310c7de205862e6634d05978d8344572'
+      'Authorization': 'token 1eb92b31310c7de205862e6634d05978d8344572',
+      'Access-Control-Allow-Origin':'*'
     }
   })
     .then(resp => resp.json())
