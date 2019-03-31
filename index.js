b7fe6a65887126f517c3afbc26456106b5be6284
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 16:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function getToken() {
 function forkRepo() {
   const repo = 'https://api.github.com/zlehmann/js-ajax-fetch-lab';
   const token = GetToken();
+  console.log(token);
   //use fetch to fork it!
   fetch(repo, {
     headers: {
