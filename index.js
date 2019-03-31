71c21989a49d33ea9d8526dbf0d43d010997cb6c
Zachary Lehmann <zlehmann@gmail.com>
2019-03-31 18:10:34 +0000

    Done.

--- index.js
+++ index.js
@@ -5,18 +5,45 @@ function getToken() {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/js-ajax-fetch-lab/forks';
   //use fetch to fork it!
+  fetch(repo, {
+    method: "POST",
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(resp => resp.json())
+    .then(json => showResults(json));
+
 }
 
 function showResults(json) {
   //use this function to display the results from forking via the API
+  console.log(json);
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = 'https://api.github.com/repos/zlehmann/js-ajax-fetch-lab/issues';
+  fetch(repo, {
+    method: "POST",
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value,
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(resp => resp.json())
+    .then(json => console.log(json));
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = 'https://api.github.com/repos/zlehmann/js-ajax-fetch-lab/issues';
+  fetch(repo, {
+    headers: {
+      Authorization: `toekn ${getToken()}`
+    }
+  })
 }
